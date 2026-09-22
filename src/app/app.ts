4c72e9c5c0bf3286ab6c7c2845623194695bde50
junayednoman <junayednoman05@gmail.com>
2026-09-22 12:53:12 +0600

    fix: new origin added to cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:8081",
       "http://10.10.28.21:5007",
+      "http://72.244.153.29:5007",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
